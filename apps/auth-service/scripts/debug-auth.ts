136a5263d9147d90dbdd811389a24f80d4a22745
Andrew Igure <andrewoigure@gmail.com>
2026-01-26 21:37:52 +0100

    fix: exclude scripts from build and remove remaining bcrypt import

--- apps/auth-service/scripts/debug-auth.ts
+++ apps/auth-service/scripts/debug-auth.ts
@@ -1,6 +1,6 @@
 
 import { PrismaClient } from '@vistalock/database';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 const prisma = new PrismaClient({
     datasources: {
